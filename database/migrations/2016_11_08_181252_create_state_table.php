46105e7f11f0992bd65650686b1ef5f174bd1dfd
HalaWKS <1028276446@qq.com>
2016-11-09 02:20:14 +0800

    数据库表迁移文件
    php artisan migrate, 可以生成这些表

---
+++ database/migrations/2016_11_08_181252_create_state_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStateTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('state', function (Blueprint $table) {
+            $table->increments('id');       //×´Ì¬ID
+            $table->string('r_id');         //·çÏÕID
+            $table->string('description');  //×´Ì¬ÃèÊö
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ database/migrations/2016_11_08_181604_create_project_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project', function (Blueprint $table) {
+            $table->increments('id');       //项目ID
+            $table->string('name');         //项目名
+            $table->string('description');  //项目描述
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
